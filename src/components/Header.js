376d4699df1731309147ee1f0b0030b75fb266ec
wellnahuel <nahuelcittadino@gmail.com>
2023-03-14 19:30:28 -0300

    3rd commit headerdone

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from 'react';
+//import images 
+import Logo from "../assets/Sun.png"
 
 const Header = () => {
-  return <div className='bg-pink-400'>Header</div>;
+  return <header className=' py-8'>
+    <div className="container mx-auto">
+      <div className="flex justify-between items-center">
+        <a href="#">
+          <img src={Logo} alt="" className="w-12"></img>
+        </a>
+        <button className="btn btn-sm"> Work with me </button>
+      </div>
+
+    </div>
+  </header>;
 };
 
 export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,7 +12,7 @@ const Nav = () => {
     <nav className= "fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
       <div className="container mx-auto">
         <div className='w-full bg-black/20  h-[66px] backdrop-blur-2xl rounded-full max-w-md mx-auto px-5 flex justify-between text-2xl text-white/50'>
-          <Link to="home" activeClass='active' smooth={true} spy={true} className="cursor-pointer w-16 h-16 flex items-center justify-center">
+          <Link to="home" activeClass='active' smooth={true} spy={true} offset={-200} className="cursor-pointer w-16 h-16 flex items-center justify-center">
             <BiHomeAlt />
           </Link>
           <Link to="about" activeClass='active' smooth={true} spy={true} className="cursor-pointer w-16 h-16 flex items-center justify-center">
@@ -42,3 +42,5 @@ export default Nav;
 //buy here in tailwind is just with : smooth={true}
 
 //scrollspy is just with : spy={true}
+
+// offset = {-200} is for when click the home button the scroll is make it to the top of the screen
