2d379659a8d083ccebd4a72270cbb38157b02fbc
wellnahuel <nahuelcittadino@gmail.com>
2023-03-15 15:22:10 -0300

    7th commit about me finish

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,96 @@
 import React from 'react';
 
+//import counter
+import CountUp from "react-countup"
+//import intersection observer hook
+import { useInView } from "react-intersection-observer"
+//import motion
+import { motion } from "framer-motion"
+//import variant
+import { fadeIn } from "../variants"
+
+
+
 const About = () => {
-  return <div className='section' id="about">About</div>;
+
+  /* esto es para que react intersection observer funcione */
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+
+  return <section className='section' id="about" ref={ref}>
+    <div className="container mx-auto">
+      <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
+        {/* image */}
+        <motion.divdiv variants={fadeIn('right', 0.3)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.3 }} className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'>
+          {/* right here the image is apply from the tailwind.config file */}
+        </motion.divdiv>
+        {/* text */}
+        <motion.div variants={fadeIn('left', 0.5)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.3 }}  className='flex-1'>
+          <h2 className='h2 text-accent'>Sobre mi</h2>
+          <h3 className='h3 mb-4'>Soy un diseñador grafico freelance con más de 10 años de experiencia.</h3>
+          <p className='mb-6'>
+            Lorem ipsum d Lorem ipsum dLorem ipsum dLorem ipsum dLorem ipsum dLorem ipsum dLorem ipsum dLorem ipsum dLorem ipsum dLorem ipsum dLorem ipsum dLorem ipsum d
+          </p>
+          {/* stats */}
+          <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
+            <div>
+              <div className=' text-[40px] font-tertiary text-gradient mb-2'>
+                { inView ? <CountUp start={0} end={12} duration={3} /> : null }
+              </div>
+              <div className='font-primary text-sm tracking-[2px]'>
+                Años de <br />
+                Experiencia
+              </div>
+            </div>
+            <div>
+              <div className=' text-[40px] font-tertiary text-gradient mb-2'>
+                { inView ? <CountUp start={0} end={100} duration={3} /> : null }
+                +
+              </div>
+              <div className='font-primary text-sm tracking-[2px]'>
+                Proyectos <br />
+                Completados
+              </div>
+            </div>
+            <div>
+              <div className=' text-[40px] font-tertiary text-gradient mb-2'>
+                { inView ? <CountUp start={0} end={1000} duration={3} /> : null }
+              </div>
+              <div className='font-primary text-sm tracking-[2px]'>
+                Clientes <br />
+                Satisfechos
+              </div>
+            </div>
+          </div>
+          <div className='flex gap-x-8 items-center'>
+            <button className='btn btn-lg'>Contáctame</button>
+            <a href="#" className='text-gradient btn-link'>
+              Mi Portafolio
+            </a>
+          </div>
+          
+        </motion.div>
+      </div>
+
+    </div>
+  </section>;
 };
 
 export default About;
 
 //attention, the class 'section' is defined in the index.css: 
 //py-8 lg:py-24 lg:h-screen flex items-center
+
+//https://www.npmjs.com/package/react-countup
+//https://www.npmjs.com/package/react-intersection-observer
+
+//the 3 values of flex are : flex-grow | flex-shrink | flex-basis
+
+// flex grow The flex-grow CSS property sets the flex grow factor of a flex item's main size.
+
+//The flex-shrink (contraccion) CSS property sets the flex shrink factor of a flex item. If the size of all flex items is larger than the flex container, items shrink to fit according to flex-shrink.
+
+//The flex-basis CSS property sets the initial main size of a flex item. It sets the size of the content box unless otherwise set with box-sizing.
+
+//mix-blend-mode The mix-blend-mode CSS property sets how an element's content should blend with the content of the element's parent and the element's background.
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 //import images
-import Image from "../assets/agosto-smaller.png"
+import Image from "../assets/agosto-circle.png"
 //import icons
 import { FaInstagram, FaYoutube, FaLinkedin } from "react-icons/fa"
 //import type of animation
@@ -13,27 +13,26 @@ import { fadeIn } from "../variants"
 
 
 const Banner = () => {
-  return <section className='section' id="home">
+  return <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id="home">
     <div className="container mx-auto">
-      <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center"> 
+      <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
         {/* text */}
         <div className="flex-1 text-center font-secondary lg:text-left">
-          <h1 className="text-[55px] font-bold leading-[0.8] lg:text-[110px] ">
+          <motion.h1 variants={fadeIn('up', 0.3)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className="text-[55px] font-bold leading-[0.8] lg:text-[110px] ">
             AGUSTÍN ORIHUELA
-          </h1>
-          <div className='mb-6 text-4xl lg:text-6xl font-secondary font-semibold leading-[1]'>
+          </motion.h1>
+          <motion.div variants={fadeIn('up', 0.4)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='mb-6 text-4xl lg:text-6xl font-secondary font-semibold leading-[1]'>
             <span className='mr-4 text-white'>
               <TypeAnimation sequence={['Diseñador Grafico', 2000, 'Brand Manager', 2000]} speed={40} wrapper='span' repeat={Infinity} className="text-pink-500" />
             </span>
-
-          </div>
-          <p className="mb-8 max-w-lg mx-auto lg:mx-0">
+          </motion.div>
+          <motion.p variants={fadeIn('up', 0.5)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className="mb-8 max-w-lg mx-auto lg:mx-0">
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          </p>
-          <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+          </motion.p>
+          <motion.div variants={fadeIn('up', 0.6)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
             <button className="btn btn-lg">Contactame</button>
             <a href="#" className="text-gradient btn-link"> Mi Portafolio</a>
-          </div>
+          </motion.div>
           {/* socials */}
           <div className="flex text-xl gap-x-6 max-w-max mx-auto lg:mx-0">
             <a href="#">
@@ -48,9 +47,9 @@ const Banner = () => {
           </div>
         </div>
         {/* image */}
-        <div>
+        <motion.div variants={fadeIn('down', 0.3)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className="hidden lg:flex flex-1 max-w-xs lg:max-w-[482px]">
           <img src={Image} alt="" />
-        </div>
+        </motion.div>
 
       </div>
     </div>
@@ -62,3 +61,12 @@ export default Banner;
 
 //attention, the class 'section' is defined in the index.css: 
 //py-8 lg:py-24 lg:h-screen flex items-center
+
+/* 
+
+animation fade
+
+ <motion.h1 variants={fadeIn('up', 0.3)} initial='hidden' whileInView={'show'} viewport={{once: false, amount: 0.7}} className="text-[55px] font-bold leading-[0.8] lg:text-[110px] ">
+            AGUSTÍN ORIHUELA
+          </motion.h1>
+*/
