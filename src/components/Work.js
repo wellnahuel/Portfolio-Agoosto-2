d9a5147cd6f50b25eff1f6afca1b94bb5b3be7c4
wellnahuel <nahuelcittadino@gmail.com>
2023-03-20 15:19:51 -0300

    11th commit component work done, but issues with box design

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,21 +4,17 @@ import { motion } from "framer-motion"
 //import variant
 import { fadeIn } from "../variants"
 // import image
-import Img1 from "../assets/1.jpg"
-import Img2 from "../assets/2.jpg"
-import Img3 from "../assets/3.jpg"
-import Img4 from "../assets/4.png"
-
-
-
-
+import Img1 from "../assets/1_crop_resize.png"
+import Img2 from "../assets/2_crop.png"
+import Img3 from "../assets/3_crop_resize.png"
+import Img4 from "../assets/4_resize.png"
 
 const Work = () => {
   return (
     <section className='section' id="work">
       <div className='container mx-auto'>
         <div className='flex flex-col lg:flex-row gap-x-10'>
-          <div className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-10'>
+          <motion.div variants={fadeIn('rigth', 0.3)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.3 }} className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-10'> {/* Titulo + descripcion + boton + 1er card */}
             {/* text */}
             <div>
               <h2 className='h2 leading-tight text-accent'>
@@ -30,8 +26,25 @@ const Work = () => {
               </p>
               <button className='btn btn-sm'>Ver todos los proyectos</button>
             </div>
-            {/* image */}
+            {/* card */}
             <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              {/* overlay */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              {/* img */}
+              <img className="group-hover:scale-125 transition-all duration-500" src={Img1} alt="" />
+              {/* pretitle */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>Algun pretitulo </span>
+              </div>
+              {/* title */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3xl text-white'>Titulo del proyecto</span>
+              </div>
+            </div>
+          </motion.div>
+          <motion.div variants={fadeIn('left', 0.3)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.3 }} className='flex-1 flex flex-col gap-y-10'> {/* segunda columna */}
+            {/* card */}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl"> {/* 2nd card */}
               {/* overlay */}
               <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
               {/* img */}
@@ -45,15 +58,13 @@ const Work = () => {
               <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
                 <span className='text-3xl text-white'>Titulo del proyecto</span>
               </div>
+              {/* image */}
             </div>
-          </div>
-          <div className='flex-1 flex flex-col gap-y-14'>
-            {/* image */}
-            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl"> {/* 3rd card */}
               {/* overlay */}
               <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
               {/* img */}
-              <img className="group-hover:scale-125 transition-all duration-500" src={Img1} alt="" />
+              <img className="group-hover:scale-125 transition-all duration-500" src={Img3} alt="" />
               {/* pretitle */}
               <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
                 <span className='text-gradient'>Algun pretitulo
@@ -64,9 +75,9 @@ const Work = () => {
                 <span className='text-3xl text-white'>Titulo del proyecto</span>
               </div>
               {/* image */}
-              
             </div>
-          </div>
+          </motion.div>
+
         </div>
       </div>
     </section>)
@@ -78,3 +89,4 @@ export default Work;
 
 //attention, the class 'section' is defined in the index.css: 
 //py-8 lg:py-24 lg:h-screen flex items-center
+
